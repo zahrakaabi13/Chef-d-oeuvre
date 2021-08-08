04f0e6cf87762e86cebdbf5692ca7d2ba43d0f1c
zahrakaabi13 <zahrakaabisimplonenda@gmail.com>
2021-08-08 09:24:37 +0100

    v1

---
+++ server.js
@@ -0,0 +1,28 @@
+//creation d'un service web de node js
+const express = require('express')
+const app = express() 
+const db = require('./config/db')
+db()
+
+
+app.use(express.json())
+const cors = require('cors')
+app.use(cors())
+
+
+const client = require('./routes/ClientRoute.js')
+const design = require('./routes/DesignRoute.js')
+const product = require('./routes/ProductRoute.js')
+
+app.use('/OBSESSION/client',client)
+app.use('/OBSESSION/desing',design)
+app.use('/OBSESSION/product',product)
+
+//cnx to server
+app.listen('4000', (err) => {
+    if(err){
+    console.log('error that prevents server to run')
+    } else {
+    console.log('Server is running on port 4000')
+}
+})
